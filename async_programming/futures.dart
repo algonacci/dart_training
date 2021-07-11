b9b0831a02f0a77b2b7be9cc5e3f72baa23a6878
algonacci <86970816+algonacci@users.noreply.github.com>
2021-07-11 23:24:05 +0700

    Add files via upload

---
+++ async_programming/futures.dart
@@ -0,0 +1,21 @@
+Future<String> fetchUserOrder() => Future.delayed(
+      Duration(seconds: 2),
+      () => 'Cappucino',
+    );
+Future<String> fetchUserOrder2() => Future.value('Espresso');
+
+Future<String> fetchUserOrder3() => Future.error(Exception('Out of milk'));
+
+Future<void> main() async {
+  print('Program Started');
+  try {
+    final order = await fetchUserOrder();
+    print(order);
+    final order2 = await fetchUserOrder2();
+    print(order2);
+  } catch (e) {
+    print(e);
+  } finally {
+    print('Done');
+  }
+}
---
+++ async_programming/stream2.dart
@@ -0,0 +1,9 @@
+void main() {
+  final stream = Stream.fromIterable([1, 2, 3]);
+  // Stream.value(10);
+  // Stream.error(Exception('Something went error'));
+  // Stream.empty();
+  // Stream.fromFuture(Future.delayed(Duration(seconds: 1), () => 42));
+  // Stream.periodic(Duration(seconds: 1), (index) => index);
+}
+
